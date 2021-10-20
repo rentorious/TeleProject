1d15f15b6cd64eb6a80f30b4eb13d6c0a8ae5047
Ognjen Popovic <rentorious@gmail.com>
2021-10-21 00:57:54 +0200

    refactor: remove  obsolete comments

--- src/App.js
+++ src/App.js
@@ -7,46 +7,6 @@ import { useLive } from "./hooks/useLive";
 const App = () => {
   const [symbols, setSymbols] = useState([]);
   const { ws, idData, symToId } = useLive(symbols);
-  // const [idData, setIdData] = useState({});
-  // const [symToId, setSymToId] = useState({});
-
-  // // Websocket events
-  // ws.onmessage = (msg) => {
-  //   const msgData = JSON.parse(msg.data);
-
-  //   setMessageCount(messageCount + 1);
-
-  //   // console.log(msgData);
-
-  //   if (msgData?.event === "subscribed") {
-  //     const { pair: sym, chanId: id } = msgData;
-
-  //     const newCrypto = {
-  //       low: 0,
-  //       high: 0,
-  //       daily_change: 0,
-  //       daily_change_rel: 0,
-  //       last_price: 0,
-  //     };
-
-  //     setSymToId({ ...symToId, [sym]: id });
-
-  //     setIdData({ ...idData, [id]: newCrypto });
-  //   } else if (msgData instanceof Array && msgData[1].length === 10) {
-  //     const id = msgData[0];
-  //     const arr = msgData[1];
-
-  //     const newData = {
-  //       low: arr[9],
-  //       high: arr[8],
-  //       daily_change: arr[4],
-  //       daily_change_rel: arr[5],
-  //       last_price: arr[6],
-  //     };
-
-  //     setIdData({ ...idData, [id]: newData });
-  //   }
-  // };
 
   // Initial setup
   useEffect(() => {
@@ -64,24 +24,6 @@ const App = () => {
       ws.close();
     };
   }, []);
-
-  // // Subscribe to selected symbols
-  // useEffect(() => {
-  //   if (symbols.length === 5) {
-  //     ws.onopen = () => {
-  //       for (const symbol of symbols) {
-  //         console.log(symbol);
-  //         let wsConfig = JSON.stringify({
-  //           event: "subscribe",
-  //           channel: "ticker",
-  //           symbol: `t${symbol.toUpperCase()}`,
-  //         });
-
-  //         ws.send(wsConfig);
-  //       }
-  //     };
-  //   }
-  // }, [symbols]);
 
   return (
     <>
